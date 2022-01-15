0d2098d9fc5374ee8ccab38352cc106ecff84a39
Sonialahyani2022 <lahyani.sonia@gmail.com>
2022-01-15 12:47:54 +0100

    first coomit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import FormComp from './components/FormComp';
+
+function App() {
+  return (
+    <div className="container">
+     <FormComp/>
+    </div>
+  );
+}
+
+export default App;
